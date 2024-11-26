bf61510b6d6408b979f0038bbe392d73b32129da
jackb60 <40903548+jackb60@users.noreply.github.com>
2024-11-26 17:05:54 -0500

    Update Main.ino

--- Central/Main.ino
+++ Central/Main.ino
@@ -9,11 +9,16 @@ Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
+Servo servo5;
+Servo servo6;
+Servo servo7;
+Servo servo8;
+Servo servo9;
+Servo servo10;
 
-byte pins[10] = {10,9,8,6,0,11,30,1,12,4};
+Servo servos[10] = {servo1, servo2, servo3, servo4, servo5, servo6, servo7, servo8, servo9, servo10};
 
-Servo servos[4] = {servo1, servo2, servo3, servo4};
-int servoPins[4] = {-1,-1,-1,-1};
+byte pins[10] = {10, 9, 8, 6, 0, 11, 30, 1, 12, 4};
 
 byte h_dir[5] = {0, 0, 0, 0, 0};
 
@@ -25,6 +30,9 @@ void setup() {
   Wire.setWireTimeout();
   
   pinMode(CONN, INPUT);
+  for(byte i = 0; i < 10; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
 
   attachInterrupt(digitalPinToInterrupt(CONN), isr, CHANGE);
 
@@ -44,10 +52,17 @@ void loop() {
 
     byte adr = Serial.read();
     if(adr == 0x00) {
-      mechanisms(Serial.read(), Serial.read(), Serial.read());
+      byte code = Serial.read();
+      byte header = Serial.read();
+      byte data = 0x00;
+      if(code != 0x05) {
+        data = Serial.read();
+      } 
+      mechanisms(code, header, data);
       Serial.write(0x01);
     }
     else if(adr & 0x01) { //ends in 1, write command
+      adr = adr >> 1;
       Wire.beginTransmission(adr);
       while(Serial.available()) {
         Wire.write(Serial.read());
@@ -81,42 +96,29 @@ void mechanisms(byte code, byte pin, byte data) {
       }
       break;
     case 0x02:
-      bool found = false;
-      for(byte i = 0; i < 4; i++) {
-        if(servoPins[i] == pins[pin]) { //if servo alr attached
-          servos[i].write(data);
-          found = true;
-        }
-      }
-      if(!found){
-        for(byte i = 0; i < 4; i++) { //if servo available and not attached
-          if(servoPins[i] == -1) {
-            servos[i].attach(pins[pin]); //assign it to the pin
-            servoPins[i] = pins[pin];
-            servos[i].write(data);
-          }
-        }
-      }
+      servos[pin].write(data);
       break;
     case 0x03:
-      h_dir[(pin/2)] = data;
+      h_dir[pin] = data;
       break;
     case 0x04:
       if(data == 0x00){
-        digitalWrite(pins[pin + 1], 0);
-        digitalWrite(pins[pin], 0);
+        digitalWrite(pins[pin * 2 + 1], 0);
+        digitalWrite(pins[pin * 2], 0);
       }
       else{
         if(h_dir[pin] == 0) {
-          digitalWrite(pins[pin + 1], 0);
-          digitalWrite(pins[pin], 1);
+          digitalWrite(pins[pin * 2 + 1], 0);
+          digitalWrite(pins[pin * 2], 1);
         }
         else {
-          digitalWrite(pins[pin], 0);
-          digitalWrite(pins[pin + 1], 1);
+          digitalWrite(pins[pin * 2], 0);
+          digitalWrite(pins[pin * 2 + 1], 1);
         }
       }
       break;
+    case 0x05:
+      servos[pin].attach(pins[pin]);
   }
 }
 
