513591a2dfcc8cc0823570ca1025d2ae6edaa674
jackb60 <40903548+jackb60@users.noreply.github.com>
2025-01-19 22:23:37 -0500

    Update Mechanisms.ino

--- Slave Boards/Mechanisms.ino
+++ Slave Boards/Mechanisms.ino
@@ -3,35 +3,29 @@
 #include <Servo.h>
 #include <Adafruit_VL53L0X.h>
 
-#define SW1
-#define SW2
-#define SW12V
-#define SW20V
-#define SERVO1
-#define SERVO2
+#define SW1 1
+#define SW2 1
+#define SW12V 1
+#define SW20V 1
+#define SERVO1 1
+#define SERVO2 1
+
 #define ADR 0x0B
+
+#define TOFTHS 10 //Threshold for detecting ball (mm)
+#define TOFCD 500 //Cooldown after detection (ms)
+
 
 byte code;
 
 short tof;
+byte launched = 0;
+unsigned long last = 0;
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
 Servo Servo1;
 Servo Servo2;
-
-// converts float to 4 byte array
-union FloatToBytes {
-  float floatValue;
-  byte byteArray[4];
-};
-FloatToBytes cell;
-
-float batteryCurrent;
-float averageCurrent = 0;
-float batteryAC;
-FloatToBytes curArray;
-FloatToBytes batArray;
 
 void setup() {
   pinMode(SW1, INPUT);
@@ -45,10 +39,11 @@ void setup() {
   Servo1.attach(SERVO1);
   Servo2.attach(SERVO2);
 
-  lox.begin();
-  lox.startRangeContinuous();
-  
   Wire.begin(ADR);
+  Wire1.begin();
+
+  lox.begin(0x29, false, &Wire1);
+  lox.startRangeContinuous();
 
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
@@ -58,77 +53,65 @@ void loop() {
   if (lox.isRangeComplete()) {
     tof = lox.readRange();
   }
+  if (tof < TOFTHS && millis() - last > TOFCD) {
+    launched += 1;
+  }
 
 }
 
-// Flipped read and write because writing to central hub
 void receiveEvent() {
-  Serial.println("REC");
   code = Wire.read();
-  // Cases:
   switch(code) {
     case 0x01:
-      byte data = Wire.read();
-      Serial.print("DTA");
-      Serial.println(data, HEX);
-      if (data == 0x00) { // Turn output on or off, byte 0x00 for off, anything else for on
-        digitalWrite(2, LOW); // turn thing off
-      } else {
-        digitalWrite(2, HIGH); // turn thing on
-      }
+      //Reset launched
+      launched = 0;
       break;
-  }
-}
-void requestEvent(){
-    Serial.println("REQ");
-    Serial.print("CODE");
-    Serial.println(code,HEX);
-    switch(code) {
     case 0x02:
-      //Voltage
-      Wire.write(batArray.byteArray, 4);
+      //Set 12V Switch PWM
+      analogWrite(SW12V, Wire.read());
       break;
     case 0x03:
-      // Current
-      Serial.print("CURRENT");
-      Serial.println(batteryCurrent);
-      Wire.write(curArray.byteArray, 4);
+      //Set 20V Switch PWM
+      analogWrite(SW20V, Wire.read());
       break;
     case 0x04:
-      // Cell #1 Voltage
-      cell.floatValue = cell1();
-      Wire.write(cell.byteArray, 4);
+      //Turn off Servo 1
+      Servo1.detach();
       break;
     case 0x05:
-      // Cell #2 Voltage
-      cell.floatValue = cell2();
-      Wire.write(cell.byteArray, 4);
+      //Turn off Servo 2
+      Servo2.detach();
       break;
     case 0x06:
-      // Cell #3 Voltage
-      cell.floatValue = cell3();
-      Wire.write(cell.byteArray, 4);
+      //Set Servo 1 to given position
+      if(!Servo1.attached()){
+        Servo1.attach(SERVO1);
+      }
+      Servo1.write(Wire.read());
       break;
     case 0x07:
-      // Cell #4 Voltage
-      cell.floatValue = cell4();
-      Wire.write(cell.byteArray, 4);
-      break;
-    case 0x08:
-      // Cell #5 Voltage
-      cell.floatValue = cell5();
-      Wire.write(cell.byteArray, 4);
-      break;
-    case 0x09:
-      // Cell #6 Voltage
-      cell.floatValue = cell6();
-      Wire.write(cell.byteArray, 4);
-      break;
-    case 0x0A:
-      // Temperature
-      FloatToBytes temp;
-      temp.floatValue = thermocouple.readFahrenheit();
-      Wire.write(temp.byteArray, 4);
-      break;
+      //Set Servo 2 to given position
+      if(!Servo2.attached()){
+        Servo2.attach(SERVO2);
+      }
+      Servo2.write(Wire.read());
+      break;      
+  }
+}
+
+void requestEvent(){
+    switch(code) {
+      case 0x08:
+        //launched
+        Wire.write(launched);
+        break;
+      case 0x09:
+        //switch 1
+        Wire.write(digitalRead(SW1));
+        break;
+      case 0x10:
+        //switch 2
+        Wire.write(digitalRead(SW2));
+        break;
   }
 }
