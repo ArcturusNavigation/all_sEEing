77559bc66a5ac05f1fda117a968ab8d9b3941e96
Noah Haefner <nhaefner@mit.edu>
2025-10-05 22:18:43 -0400

    WIP Add analog read

--- Shore_Boards/XBEE_SHORE_ESTOP/XBEE_SHORE_ESTOP.ino
+++ Shore_Boards/XBEE_SHORE_ESTOP/XBEE_SHORE_ESTOP.ino
@@ -29,6 +29,21 @@ byte x2val;
 byte y1val;
 byte y2val;
 
+
+typedef enum {MODE_MAN=0, MODE_SEMIMAN=2, MODE_AUTO=1} drive_mode;
+drive_mode get_drive_mode(drive_mode_pin) {
+  // TODO make sure this works with autonomy
+	// Use a resistor divider to get multiple states on this pin
+	const int thresh1 = 1024/3;
+	const int thresh2 = 2*1024/3;
+	int drive_val = analogRead(drive_mode_pin);
+
+	return drive_val <=  thresh1 ? MODE_MAN : 
+			drive_val >= thresh2 ? MODE_AUTO : 
+			MODE_SEMIMAN;
+}
+
+
 void setup() {
   pinMode(STATUS, OUTPUT);
   pinMode(BATT, OUTPUT);
@@ -43,8 +58,11 @@ void setup() {
   Serial2.begin(115200);
 }
 
+
 void loop() {
-  status = !digitalRead(ESTOP) + (digitalRead(MANUAL) << 1);
+  // Status in the form of (Semiman, Auto/man, ESTOP)
+  // TODO figure out what is 1 and 0
+  status = !digitalRead(ESTOP) + (get_drive_mode(MANUAL) << 1);
   x1val = 127;
   y1val = 127;
   x2val = 127;
@@ -62,8 +80,8 @@ void loop() {
     y2val = map(analogRead(Y2) - y2cal, -y2cal, 1023 - y2cal, 0, 254);
   }
 
-  uint8_t chksum = (x1val + y1val + x2val + y2val) & 0b11111;
-  status += chksum << 2;
+  uint8_t chksum = (x1val + y1val + x2val + y2val) & 0b1111;
+  status += chksum << 3;
   Serial.write(0xFF);
   Serial.write(status);
   Serial.write(x1val);
